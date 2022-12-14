5f8496d5a3ec96d64f55bb35a7dbe64ef953e6b8
张亚飞 <672233580@qq.com>
2022-12-14 14:41:42 +0800

    Initial Commit

---
+++ IsometricAnimation1214/IsometricAnimation1214App.swift
@@ -0,0 +1,17 @@
+//
+//  IsometricAnimation1214App.swift
+//  IsometricAnimation1214
+//
+//  Created by 张亚飞 on 2022/12/14.
+//
+
+import SwiftUI
+
+@main
+struct IsometricAnimation1214App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
